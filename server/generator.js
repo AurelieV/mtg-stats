08ea3a0ed698badbff9e7f18894e014d800cd487
Aurélie VIOLETTE <orwel.violette@gmail.com>
2017-04-17 15:14:10 +0200

    Add elasticsearch script

---
+++ server/generator.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const tmp = require("../AKH-tmp.json")
+
+const colors = {
+    W: "White",
+    B: "Black",
+    U: "Blue",
+    R: "Red",
+    G: "Green"
+};
+const rarity = {
+    U: "Uncommon",
+    C: "Common",
+    R: "Rare",
+    M: "Mythic"
+};
+const cards = tmp.map((c, i) => {
+    const card = {
+        name: c.Name,
+        cmc: c.Converted,
+        supertypes: c.Supertypes.split(" ").filter(t => t),
+        subtypes: c.Subtypes.split(" ").filter(t => t),
+        types: c.Types.split(" "),
+        colors: c.Color !== "X" ? Array.from(c.Color).map(c => colors[c]) : null,
+        rarity: rarity[c.Rarity],
+        multiverseid: i
+    }
+    if (card.types.indexOf("creature") > -1) {
+        const [power, toughness] = c.Text.split("\n")[0].split("/");
+        card.power = power;
+        card.toughness = toughness;
+        card.text = c.Text.split("\n").slice(1).join("\n")
+    } else {
+        card.text = c.Text
+    }
+
+    return card;
+});
+const AKH = {
+    name: "Amonkhet",
+    code: "AKH",
+    cards
+};
+
+fs.writeFileSync("./tmp.json", JSON.stringify(AKH, null, 4));
